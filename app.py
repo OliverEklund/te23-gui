e3a68e2a7b07547048e13bf8513d217c8812675f
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-03-27 09:46:54 +0100

    message

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from tkinter import *
 from random import randint
 
+dice_sides = 6
+
+def show(): 
+    button.config( text = 'clicked.get()' ) 
+
 def roll():
-    roll=randint(1,6)
-    text_box.config(text=roll)
+    roll=randint(1,dice_sides)
+    text_box_1.config(text=roll)
+
+
+options = [ 
+    'so cool'
+] 
+
 
 root = Tk()
 root.title("Cool tärning")
 
-text_box = Label(root, text = 0)
-text_box.pack()
+clicked = StringVar() 
+
+text_box_1 = Label(root, text = 0)
+text_box_1.pack()
 
 roll_button = Button(root, text='Slå tärning', width=25, command=roll)
 roll_button.pack()
 
-nuke_button = Button(root, text='Nuke the moon', width=25, command=root.)
+nuke_button = Button(root, text='Bomb', width=25, command=root.destroy)
 nuke_button.pack()
 
-button = Button(root, text='Quit', width=25, command=root.destroy)
-button.pack()
+drop = OptionMenu( root , clicked , *options ) 
+drop.pack() 
 
 root.mainloop()
