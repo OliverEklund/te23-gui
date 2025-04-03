f9290521b549f46b40cbe84b0a35aaf275b280cd
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-04-03 08:55:53 +0200

    h

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from tkinter import *
 from random import randint
 
 dice_sides = 6
+high_score = 0
 
 def show(): 
     button.config( text = 'clicked.get()' ) 
 
+def update_dice_sides(*args):
+    global dice_sides
+    dice_sides = int(clicked.get())
+
 def roll():
     roll=randint(1,dice_sides)
     text_box_1.config(text=roll)
-
 
 options = [ 
     2,
@@ -25,6 +29,8 @@ root = Tk()
 root.title("Cool tärning")
 
 clicked = StringVar()
+clicked.set(options[0]) 
+clicked.trace("w", update_dice_sides) 
 
 text_box_1 = Label(root, text = 0)
 text_box_1.pack()
@@ -35,6 +41,10 @@ roll_button.pack()
 nuke_button = Button(root, text='Bomb', width=25, command=root.destroy)
 nuke_button.pack()
 
-drop = OptionMenu( root , clicked , *options ) 
+drop = OptionMenu( root , clicked , *options )
+
+text_box_2 = Label(root, text = high_score )
+text_box_2.pack()
+
 drop.pack() 
 root.mainloop()
